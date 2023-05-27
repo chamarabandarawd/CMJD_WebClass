0ec2be605dde6eb6e63e3df4de6e1be4aa95e09e
chamarabandarawd <chamarabandarawd@gmail.com>
2023-05-27 09:36:22 +0530

    add Routes and Route int APP.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import HomePage from './Pages/HomePage/HomePage';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
+import HomePage from './Pages/HomePage/HomePage';
+import AboutPage from './Pages/AboutPage/AboutPage';
+import AppNav from './Component/AppNav/AppNav';
+import { Route, Routes } from 'react-router-dom';
+
+
 
 function App() {
   return (
    <>
-   <HomePage/>
+  <AppNav/>
+  <Routes>
+    <Route path="/" Component={HomePage}/>
+    <Route path="/about" Component={AboutPage}/>
+  </Routes>
+
    </>
   );
 }
--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -1,7 +1,5 @@
 import React, { useState } from 'react';
-import LargeButton from '../../Component/LargeButton/LargeButton';
-import AppNav from '../../Component/AppNav/AppNav';
-import TextFeild from '../../Component/TextFeild/TextFeild';
+
 
 
 export default function HomePage() {
