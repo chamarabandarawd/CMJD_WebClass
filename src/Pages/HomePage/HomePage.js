55ba4bed218baa94ca0569994fb09a43341ca4e6
chamarabandarawd <chamarabandarawd@gmail.com>
2023-05-27 08:51:39 +0530

    fetch API and map function

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -5,13 +5,34 @@ import TextFeild from '../../Component/TextFeild/TextFeild';
 
 
 export default function HomePage() {
-  const[email,setEmail]=useState("");
+  const [email, setEmail] = useState("");
+  const [data, setData] = useState([]);
+
+  const getData = async () => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then((response) => response.json())
+    .then((json) => setData(json));
+  }
+
   return (
     <div>
-        <input value={email} onChange={(e)=>{
-          setEmail(e.target.value);
-          console.log(email);
-        }} placeholder="Email"/>
+      <input value={email} onChange={(e) => {
+        setEmail(e.target.value);
+        console.log(email);
+      }} placeholder="Email" />
+
+      <button onClick={getData}>Get data</button>
+
+      {
+        data.map(data=>{
+          return(
+            <div className='card'>
+              <h2>{data.title}</h2>
+              <p>{data.body}</p>
+            </div>
+          )
+        })
+      }
     </div>
   )
 }
